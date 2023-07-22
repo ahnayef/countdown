e8fb79018b517132f41d4e45dabe2156fb4c766e
AHNayef <ahnayef@outlook.com>
2023-07-22 21:01:44 +0600

    Using state to update

--- app/(components)/countdown/page.tsx
+++ app/(components)/countdown/page.tsx
@@ -1,40 +1,48 @@
-"use client";
+"use client"
 
 import { useState, useEffect } from 'react';
 import style from './countdown.module.css';
 
 export default function Countdown(props: any) {
-    const { targetDate, title } = props;
+  const { targetDate, title } = props;
 
-    const [seconds, setSeconds] = useState(0);
+  const [difference, setDifference] = useState(0);
+  const [weeks, setWeeks] = useState(0);
+  const [days, setDays] = useState(0);
+  const [hours, setHours] = useState(0);
+  const [minutes, setMinutes] = useState(0);
+  const [seconds, setSeconds] = useState(0);
 
-    let date1 = new Date();
-    let date2 = new Date(targetDate);
-    let difference = date2.getTime() - date1.getTime();
-    
-    let weeks = Math.floor(difference / (1000 * 60 * 60 * 24 * 7));
-    let days = Math.floor((difference / (1000 * 60 * 60 * 24)) % 7);
-    let hours = Math.floor((difference / (1000 * 60 * 60)) % 24);
-    let minutes = Math.floor((difference / (1000 * 60)) % 60);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      let date1 = new Date();
+      let date2 = new Date(targetDate);
+      let difference = date2.getTime() - date1.getTime();
+      let weeks = Math.floor(difference / (1000 * 60 * 60 * 24 * 7));
+      let days = Math.floor((difference / (1000 * 60 * 60 * 24)) % 7);
+      let hours = Math.floor((difference / (1000 * 60 * 60)) % 24);
+      let minutes = Math.floor((difference / (1000 * 60)) % 60);
+      let seconds = Math.floor((difference / 1000) % 60);
 
+      setDifference(difference);
+      setWeeks(weeks);
+      setDays(days);
+      setHours(hours);
+      setMinutes(minutes);
+      setSeconds(seconds);
+    }, 1000);
 
-    useEffect(() => {
-        setSeconds(Math.floor((difference / 1000) % 60));
-        const interval = setInterval(() => {
-            setSeconds((prevSeconds) => prevSeconds - 1);
-        }, 1000);
+    return () => clearInterval(interval);
+  }, [targetDate]);
 
-        return () => clearInterval(interval);
-    }, [targetDate]);
-
-    return (
-        <div className={style.countdownMain}>
-            <h1 className={style.title}>{title}</h1>
-            <div className={style.countdown}>
-                {difference < 0 ? "The time has come!" :
-                <div>{weeks} weeks {days} days {hours} hours {minutes} minutes {seconds} seconds</div>
-}
-            </div>
-        </div>
-    );
+  return (
+    <div className={style.countdownMain}>
+      <h1 className={style.title}>{title}</h1>
+      <div className={style.countdown}>
+        {difference < 0 ? "The time has come!" :
+        <div>{weeks} weeks {days} days {hours} hours {minutes} minutes {seconds} seconds</div>
+        }
+      </div>
+    </div>
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,15 @@
 import styles from './page.module.css'
 import Countdown from './(components)/countdown/page'
+import data from "./assets/target.json"
 
 
 export default function Home() {
 
-  const targetDate = new Date("2023-09-14T12:00");
+  // const targetDate = new Date("2023-09-14T12:00");
 
   return (
     <main className={styles.main}>
-      <Countdown title={"ahn"} targetDate={targetDate}/>
+      <Countdown title={data.title} targetDate={data.targetDate} />
     </main>
   )
 }
